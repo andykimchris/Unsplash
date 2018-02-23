ff01166ff47e94517efa5230a59ba6adb500204d
Andy <andrew.muchiri97@gmail.com>
2018-02-23 11:37:50 +0300

    adds index function to display index page

--- unsplash/images/views.py
+++ unsplash/images/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import Http404
 
 # Create your views here.
+
+
+def index(request):
+
+    title = 'Unsplash Images'
+    context = {
+        "title": title
+    }
+    return render(request, 'index.html', context)
