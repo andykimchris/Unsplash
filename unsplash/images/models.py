142b23ac6fb1ae1bd2e1ed91f6bf7ec7db21da47
Andy <andrew.muchiri97@gmail.com>
2018-02-23 11:43:42 +0300

    adds Gallery class to add images with foreign keys set up for other models

--- unsplash/images/models.py
+++ unsplash/images/models.py
@@ -27,3 +27,22 @@ class Category(models.Model):
     def search_by_category(cls, search_category):
         category = cls.objects.filter(category__icontains=search_category)
         return category
+
+
+class Gallery(models.Model):
+    image = models.ImageField(upload_to='gallery/', null=True, blank=True,
+                              width_field="width_field", height_field="height_field")
+    width_field = models.IntegerField(default=0)
+    height_field = models.IntegerField(default=0)
+    image_name = models.CharField(max_length=25)
+    description = models.TextField(max_length=100)
+    location = models.ForeignKey(Location)
+    category = models.ForeignKey(Category)
+
+    def __str__(self):
+        return self.image_name
+
+    @classmethod
+    def my_images(cls):
+        images = cls.objects.all()
+        return images
