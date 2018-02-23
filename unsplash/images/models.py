8555a9a774dd996f8c51e068f867a5f2d7d38b14
Andy <andrew.muchiri97@gmail.com>
2018-02-23 11:40:58 +0300

    adds Location model to take location data of the images

--- unsplash/images/models.py
+++ unsplash/images/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Location(models.Model):
+    place = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.place
+
+    class Meta:
+        ordering = ['place']
+
+    def save_location(self):
+        self.save()
