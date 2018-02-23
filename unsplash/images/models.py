62c122cea847462cdecbb5cf3f37783fb5b777f4
Andy <andrew.muchiri97@gmail.com>
2018-02-23 11:41:39 +0300

    adds category model to hold different categories for the images

--- unsplash/images/models.py
+++ unsplash/images/models.py
@@ -12,3 +12,18 @@ class Location(models.Model):
 
     def save_location(self):
         self.save()
+
+
+class Category(models.Model):
+    category = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.category
+
+    def save_category(self):
+        self.save()
+
+    @classmethod
+    def search_by_category(cls, search_category):
+        category = cls.objects.filter(category__icontains=search_category)
+        return category
