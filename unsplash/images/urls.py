97e4d1bd44f60c0978a4edbe2713c30564dee7b2
Andy <andrew.muchiri97@gmail.com>
2018-02-24 21:54:57 +0300

    adds search url and image url with their connecting view functions

--- unsplash/images/urls.py
+++ unsplash/images/urls.py
@@ -5,7 +5,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<id>\d+)/$', views.show_image_id, name="image"),
+    url(r'^image/(?P<id>\d+)/$', views.show_image_id, name="image"),
+    url(r'^search/', views.search, name='search'),
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
