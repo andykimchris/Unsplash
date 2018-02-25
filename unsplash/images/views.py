060eff0c04e7e7ec65a8c02cba9223ac983935d5
Andy <andrew.muchiri97@gmail.com>
2018-02-25 20:26:20 +0300

    adds delete image function that clears an image from the database

--- unsplash/images/views.py
+++ unsplash/images/views.py
@@ -16,6 +16,21 @@ def index(request):
     return render(request, 'index.html', context)
 
 
+def delete_image(request, id):
+
+    image_id = Gallery.objects.get(id=id)
+
+    if request.method == "POST":
+        delete_image = image.get_delete_url()
+        image.remove()
+        return redirect(delete_image)
+
+    context = {
+        "image_id": image_id
+    }
+    return render(request, 'images.html', context)
+
+
 def show_image_id(request, id):
 
     image_id = Gallery.objects.get(id=id)
@@ -25,25 +40,21 @@ def show_image_id(request, id):
     return render(request, 'images.html', context)
 
 
-def search_results(request):
+def search(request):
 
-    if 'category' in request.GET and request.GET['q']:
+    if 'category' in request.GET and request.GET['category']:
         search_term = request.GET.get('category')
-        category = Category.search_by_category(search_term)
+        category = Category.search_by_image(search_term)
         message = f"{search_term}"
-        images = Gallery.my_images()
 
         context = {
             "category": category,
             "message": message,
-            "images": images
-
         }
         return render(request, 'search.html', context)
 
     else:
         message = "You haven't searched for any term"
-
         context = {
             "message": message
         }
