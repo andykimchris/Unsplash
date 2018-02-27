406310e24a2ce1a8dad2db9421ea4ffe275e9393
Andy <andrew.muchiri97@gmail.com>
2018-02-27 17:18:44 +0300

    adds url patterns for location and category

--- images/urls.py
+++ images/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^image/(?P<id>\d+)/$', views.show_image_id, name="image"),
     url(r'^image/(?P<id>\d+)/delete/$', views.delete_image, name="delete_image"),
-    url(r'^search/', views.search, name='search')
+    url(r'^search_results/', views.search_results, name='search_results'),
+    url(r'^search/location/', views.search_location, name='search_location')
 
 ]
 if settings.DEBUG:
